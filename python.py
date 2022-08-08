a662263e02e7e4c0b3a9dc19fe71808799e71716
ZIIKUN2703 <110508974+ZIIKUN2703@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello world")
+print("Nama saya ramzi")
+print("saya penyuka anime")
+print("bau bawang")
+print("waifu saya fubuki")
+print("cita cita saya jadi animehj")
+print("anime favorit saya one piece")
+print("saya belum pernah glory")
+print("saya jangkung")
+print("saya sipit tapi bukan orang cina")
---
+++ python2.py
@@ -0,0 +1,14 @@
+# Belajar Input
+
+nama = "Ramzi" # Ramzi adalah data, nama adalah variabel
+umur = 15 
+alamat = "Cimanggis Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+
+print("Nama saya adalah " + nama + " umur saya " +str(umur) +" alamat saya " + alamat)
+
+print("Nama saya adalah " + nama )
