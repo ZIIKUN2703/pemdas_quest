ca008db1f3f8f047c2a72ec86335954bd26d6ed9
ZIIKUN2703 <110508974+ZIIKUN2703@users.noreply.github.com>
2022-08-11 20:43:20 -0700

    Add files via upload

---
+++ UPRAK_Muhamad Ramzi_X TKJ 1.py
@@ -0,0 +1,8 @@
+nama = input("Masukkan nama anda: ")
+
+nama_belakang = input("Masukkan nama belakang anda: ")
+print("Nama anda adalah", nama, "dan nama belakang anda adalah", nama_belakang)
+
+suhu = int(input("Masukkan suhu celcius: "))
+reamur = (4/5 * suhu) + 32
+print(suhu, "celcius =", reamur , "reamur")
